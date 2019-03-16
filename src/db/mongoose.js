dbd29f4154d9e550d2e33cd67f36e5abb21d1905
Harman Bains <bains.hmn@gmail.com>
2019-03-15 19:57:06 -0700

    fixing mongodb connection error

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+console.log(process.env.MONGODB_URI)
+console.log('HEY HEY HEY')
 
 //Connect to DataBase
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
